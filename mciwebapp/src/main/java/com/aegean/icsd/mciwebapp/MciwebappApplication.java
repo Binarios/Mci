1ff6fb3229bd95c1c9b4f862f45a291cb90d1847
binarios01 <i.igoumenakis@gmail.com>
2019-07-07 19:21:13 +0200

    spring boot init

---
+++ mciwebapp/src/main/java/com/aegean/icsd/mciwebapp/MciwebappApplication.java
@@ -0,0 +1,13 @@
+package com.aegean.icsd.mciwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MciwebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MciwebappApplication.class, args);
+  }
+
+}
