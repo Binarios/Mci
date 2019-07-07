b4aa709898ce82906b3d44f770a695f3de5d7cfc
binarios01 <i.igoumenakis@gmail.com>
2019-07-07 15:56:05 +0200

    travis

--- clone-games-ontology.sh
+++ clone-games-ontology.sh
@@ -8,7 +8,7 @@ echo "> Working dir: $WORKING_DIR"
 ls -la
 
 echo "> Getting data..."
-git clone git@github.com:Binarios/MciOntology.git
+git clone https://github.com/Binarios/MciOntology.git
 
 #echo "> Making data dir"
 #sudo mv your-data-repo data
