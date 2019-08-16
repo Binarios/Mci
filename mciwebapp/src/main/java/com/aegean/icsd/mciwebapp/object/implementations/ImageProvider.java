02a0d5543c8d0c2aca4d1e023a6d4fd6574407fc
iigoumenakis <ioannis.igoumenakis@amadeus.com>
2019-08-16 16:05:55 +0200

    refactor

--- mciwebapp/src/main/java/com/aegean/icsd/mciwebapp/object/implementations/ImageProvider.java
+++ mciwebapp/src/main/java/com/aegean/icsd/mciwebapp/object/implementations/ImageProvider.java
@@ -1,5 +1,11 @@
 package com.aegean.icsd.mciwebapp.object.implementations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +19,11 @@ import com.aegean.icsd.mciwebapp.object.beans.Image;
 import com.aegean.icsd.mciwebapp.object.beans.ProviderException;
 import com.aegean.icsd.mciwebapp.object.beans.Word;
 import com.aegean.icsd.mciwebapp.object.configurations.ImageConfiguration;
+import com.aegean.icsd.mciwebapp.object.dao.IObjectsDao;
 import com.aegean.icsd.mciwebapp.object.interfaces.IImageProvider;
 import com.aegean.icsd.mciwebapp.object.interfaces.IObjectFileProvider;
 import com.aegean.icsd.mciwebapp.object.interfaces.IWordProvider;
+import com.aegean.icsd.ontology.interfaces.IMciModelReader;
 
 @Service
 public class ImageProvider implements IImageProvider {
@@ -35,19 +43,48 @@ public class ImageProvider implements IImageProvider {
   private IWordProvider wordProvider;
 
   @Autowired
+  private IObjectsDao dao;
+
+  @Autowired
   private IObjectFileProvider fileProvider;
 
+  @Autowired
+  private IMciModelReader model;
+
   @Override
-  public Image getImage() throws ProviderException {
-    LOGGER.info("Retrieving an Image");
+  public Image selectImageByNode(String nodeName) throws ProviderException {
+    String id = model.removePrefix(nodeName);
     Image img = new Image();
+    img.setId(id);
+    try {
+      generator.selectObj(img);
+    } catch (EngineException e) {
+      throw Exceptions.UnableToGetWord("node name = " + nodeName, e);
+    }
+    return img;
+  }
 
-    String line = fileProvider.getFileLineFromUrl(config.getLocation() + "/" + config.getFilename());
+  @Override
+  public List<Image> selectImagesByEntityId(String entityId) throws ProviderException {
+    List<String> ids = dao.getAssociatedImagesOfId(entityId);
+    List<Image> images = new ArrayList<>();
+    for (String id : ids) {
+      Image image = new Image();
+      image.setId(id);
+      try {
+        generator.selectObj(image);
+        images.add(image);
+      } catch (EngineException e) {
+        throw Exceptions.UnableToGetWord("entityId = " + entityId, e);
+      }
+    }
+    return images;
+  }
 
-    String[] fragments = line.split(config.getDelimiter());
-    String url = fragments[config.getUrlIndex()];
-    img.setPath(url);
 
+  @PostConstruct
+  void readImages() throws ProviderException {
+    List<String> lines = fileProvider.getLines(config.getLocation() + "/" + config.getFilename());
     EntityRestriction imageSubjRes;
     EntityRestriction imageTitleRes;
     try {
@@ -57,24 +94,27 @@ public class ImageProvider implements IImageProvider {
       throw Exceptions.GenerationError(Image.NAME, e);
     }
 
-    String title = fragments[config.getTitleIndex()];
-    String subject = fragments[config.getSubjectIndex()];
+    for (String line : lines) {
+      String[] fragments = line.split(config.getDelimiter());
+      String url = fragments[config.getUrlIndex()];
+      Image img = new Image();
+      img.setPath(url);
+      String title = fragments[config.getTitleIndex()];
+      String subject = fragments[config.getSubjectIndex()];
 
-    try {
-      generator.selectObj(img);
-      if (img.getId() == null) {
-        generator.upsertGameObject(img);
+      try {
+        generator.selectObj(img);
+        if (img.getId() == null) {
+          generator.upsertGameObject(img);
+        }
+
+        Word titleWord = wordProvider.getWordWithValue(title);
+        Word subjectWord = wordProvider.getWordWithValue(subject);
+        generator.createObjRelation(img.getId(), imageTitleRes.getOnProperty(), titleWord.getId());
+        generator.createObjRelation(img.getId(), imageSubjRes.getOnProperty(), subjectWord.getId());
+      } catch (EngineException e) {
+        throw Exceptions.GenerationError(Image.NAME, e);
       }
-
-      Word titleWord = wordProvider.getWordWithValue(title);
-      Word subjectWord = wordProvider.getWordWithValue(subject);
-      generator.createObjRelation(img.getId(), imageTitleRes.getOnProperty(), titleWord.getId());
-      generator.createObjRelation(img.getId(), imageSubjRes.getOnProperty(), subjectWord.getId());
-
-      LOGGER.info(String.format("Retrieving an Image with id %s", img.getId()));
-      return img;
-    } catch (EngineException e) {
-      throw Exceptions.GenerationError(Image.NAME, e);
     }
   }
 }
--- mciwebapp/src/main/java/com/aegean/icsd/mciwebapp/object/interfaces/IImageProvider.java
+++ mciwebapp/src/main/java/com/aegean/icsd/mciwebapp/object/interfaces/IImageProvider.java
@@ -1,8 +1,14 @@
 package com.aegean.icsd.mciwebapp.object.interfaces;
 
+import java.util.List;
+
 import com.aegean.icsd.mciwebapp.object.beans.Image;
 import com.aegean.icsd.mciwebapp.object.beans.ProviderException;
 
 public interface IImageProvider {
+  Image selectImageByNode(String nodeName) throws ProviderException;
+
+  List<Image> selectImagesByEntityId(String entityId) throws ProviderException;
+
   Image getImage() throws ProviderException;
 }
